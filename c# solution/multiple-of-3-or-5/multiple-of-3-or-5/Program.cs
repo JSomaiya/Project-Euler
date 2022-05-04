e47c45e93894532713646ca8684b3e9bc26fcaff
Jai Somaiya <jai_somaiya@hotmail.co.uk>
2022-05-04 11:50:26 +0100

    c# solution

---
+++ c# solution/multiple-of-3-or-5/multiple-of-3-or-5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace multiple_of_3_or_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    total += i;
+                }
+            }
+
+            Console.Write(total);
+            Console.Read();
+        }
+    }
+}
